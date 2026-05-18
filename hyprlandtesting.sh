87a558781fb42eb9ff10be211155ced16dd44529
TotallyDIO <hunter.tadlock911@gmail.com>
2026-05-18 16:38:30 -0500

    tee hee

--- hyprlandtesting.sh
+++ hyprlandtesting.sh
@@ -47,7 +47,7 @@ mkdir -p "$WORK_DIR" "$OUT_DIR"
 # Copy base archiso profile
 # ==============================================================================
 info "Copying archiso releng profile..."
-cp -r ./ezreleng "$WORK_DIR/"
+cp -r ./archiso-work/ezreleng "$WORK_DIR/"
 chmod u+w "$PROFILE_DIR/profiledef.sh"
 
 # ==============================================================================
